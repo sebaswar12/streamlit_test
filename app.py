8a3141f09047d49e977aae2e66d4db3db90e534e
sebaswar12 <jsebastianjara@gmail.com>
2025-07-11 09:07:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,11 @@ import streamlit as st
 import pandas as pd
 import scipy as sp
 st.header('Lanzar una moneda')
-st.write('Esta aplicacion aun no esta funcional')
+
+number_of_trials = st.slider('¿Número de intentos?', 1, 1000, 10)
+start_button = st.button('Ejecutar')
+
+if start_button:
+    st.write(f'Experimento con {number_of_trials} intentos en curso.')
+
+st.write('Esta aplicación aún no es funcional. En construcción.')
